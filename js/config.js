e31c15b471da03531355a2e587038521831efa09
CalebZeng <94126509+Shawnty-z@users.noreply.github.com>
2024-09-06 15:28:59 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-central-1_CAWhTeSUX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6qpvh2c1kjsvb0vq4s1gpvh52t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
