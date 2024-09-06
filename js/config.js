ac8b63f66f947c0afa759ff7b4b9c02eb72a5d0c
CalebZeng <94126509+Shawnty-z@users.noreply.github.com>
2024-09-06 15:29:35 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-central-1_CAWhTeSUX', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6qpvh2c1kjsvb0vq4s1gpvh52t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
